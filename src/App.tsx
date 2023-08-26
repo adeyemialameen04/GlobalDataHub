cc3eec03e48789a5ca98f67bb2e9e966f0faef81
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-08-26 15:33:17 +0100

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { useEffect } from "react"
 import { API_KEY, BASE_URL } from "./utils/constants"
 import axios from "axios"
+import Navbar from "./Components/Navbar/Navbar"
 
 function App() {
 
-  useEffect(()=>{
-    const getCountriesData =async()=>{
-      try {
-        const url = `${BASE_URL}all?access_key=${API_KEY}`
-        const response  = await axios.get(url)
-        const data:[] = await response.data
-        console.log(data)
-      } catch (error) {
-        console.error(error)
-      }
-    }
+  // useEffect(()=>{
+  //   const getCountriesData =async()=>{
+  //     try {
+  //       const url = `${BASE_URL}all?access_key=${API_KEY}`
+  //       const response  = await axios.get(url)
+  //       const data:[] = await response.data
+  //       console.log(data)
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //   }
 
-    getCountriesData()
-  },[])
+  //   getCountriesData()
+  // },[])
 
   return (
     <>
+    <Navbar/>
       Hello hows u
     </>
   )
